4e24d0c67fb64d1dac0d5f2a92e536b728e2bee8
puistaja <kpuistaja@gmail.com>
2021-05-26 21:38:06 +0300

    update code for scraping 10 photos

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({headless: false, defaultViewport: {width: 1920, height: 1080}});
   const page = await browser.newPage();
   await page.goto('https://invisiblebread.com');
-  await page.waitForSelector('#comic-1');  
-  const element = await page.$('#comic-1');       
-  await element.screenshot({path: '1.png'})
+  for(let i = 1; i<11; i++){
+    page.on('load', () => console.log("Loaded: " + page.url())); // page is loaded before taking screenshot
+    await page.waitForSelector('#comic-1');  
+    const element = await page.$('#comic-1');       
+    await element.screenshot({path: `images/${i}.png`});
+    await page.waitForSelector('.navi.navi-prev');
+    await page.click('.navi.navi-prev');
+  }
 
   await browser.close();
 })();
